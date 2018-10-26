e259fd3ff612603afe5039df8f73b39b30095b65
baohe-zhang <baohez2@illinois.edu>
2018-10-26 01:56:12 -0500

    update

--- sender/sender.go
+++ sender/sender.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"math/rand"
 	"net"
+	"strconv"
 	"time"
 )
 
 const (
-	receiverListenAddr = "127.0.0.1:8080"
-	senderListenAddr   = "127.0.0.1:20002"
+	SYN        = 0X01 << 1
+	ACK        = 0X01 << 2
+	FIN        = 0X01 << 3
+	SourceIP   = "127.0.0.1"
+	SourcePort = 20002
+	TargetIP   = "127.0.0.1"
+	TargetPort = 8080
+	HeaderLength = 18
 )
 
 type Header struct {
@@ -21,8 +30,19 @@ type Header struct {
 	winSize uint16
 }
 
+var curSeqNum uint32
+
+func generateSeqNum() uint32 {
+	randSource := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(randSource)
+
+	seqNum := r.Intn(1 << 16)
+
+	return uint32(seqNum)
+}
+
 func sendUDP(packet []byte) {
-	conn, err := net.Dial("udp", receiverListenAddr)
+	conn, err := net.Dial("udp", TargetIP+":"+strconv.Itoa(TargetPort))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -32,7 +52,7 @@ func sendUDP(packet []byte) {
 }
 
 func recvUDP() {
-	pc, err := net.ListenPacket("udp", senderListenAddr)
+	pc, err := net.ListenPacket("udp", TargetIP+":"+strconv.Itoa(TargetPort))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -43,37 +63,89 @@ func recvUDP() {
 		packet := make([]byte, 1024)
 		n, addr, err := pc.ReadFrom(packet)
 		if err != nil {
-		fmt.Println(err.Error())
+			fmt.Println(err.Error())
 		}
 
 		// Handle receving packet
-		fmt.Println(n, addr.String(), packet)
+		fmt.Printf("%s %v", addr.String(), packet[:n])
+		packetHandler(packet[:n])
 	}
 }
 
-// Initiate to establish connection
+func packetHandler(packet []byte) {
+	// Deserialize header
+	var header Header
+	headerBytes := packet[:HeaderLength]
+	headerBuf := bytes.NewReader(headerBytes)
+	err = binary.Read(headerBuf, binary.BigEndian, &header)
+	if err != nil {
+			fmt.Println(err.Error())
+	}
+
+	if header.flags & SYN != 0 {
+
+	}
+
+	if header.flags & ACK != 0 {
+
+	}
+
+	if header.flags & FIN != 0 {
+		
+	}
+
+	if (len(packet) > HeaderLength) {
+		payload := packet[HeaderLength:]
+		fmt.Printf("%v", payload)
+	}
+}
+
+
+// Initiate a connection
 func initConn() {
+	curSeqNum = generateSeqNum()
+
+	header := Header{
+		srcIP:   binary.BigEndian.Uint32(net.ParseIP(SourceIP).To4()),
+		srcPort: SourcePort,
+		seqNum:  curSeqNum,
+		ackNum:  0x00,
+		flags:   SYN,
+		winSize: uint16(4096),
+	}
+
+	// Serialize the header
+	headerBuf := bytes.Buffer{}
+	binary.Write(&headerBuf, binary.BigEndian, header)
+
+	sendUDP(headerBuf.Bytes())
+}
+
+func closeConn() {
 
 }
 
-func generateSeq() uint16 {
-	randSource := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(randSource)
+func sendData() {
+	header := Header{
+		srcIP:   binary.BigEndian.Uint32(net.ParseIP(SourceIP).To4()),
+		srcPort: SourcePort,
+		seqNum:  curSeqNum,
+		ackNum:  0x00,
+		flags:   0x00,
+		winSize: uint16(4096),
+	}
 
-	seqNum := r.Intn(0x01 << 15)
+	// Serialize the header
+	headerBuf := bytes.Buffer{}
+	binary.Write(&headerBuf, binary.BigEndian, header)
 
-	return uint16(seqNum)
+	sendUDP(headerBuf.Bytes())
+}
+
+func sendACK(_header Header) {
+
 }
 
 func main() {
-	pkt := []byte("Hello Test")
-	sendUDP(pkt)
+	initConn()
 }
-
-
-
-
-
-
-
-
